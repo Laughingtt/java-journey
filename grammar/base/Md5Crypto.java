2fb69f66766a3ad3770c170b676a180a10ec1b58
Laughing <tianjian361@163.com>
2023-11-20 17:45:35 +0800

    update base

---
+++ grammar/base/Md5Crypto.java
@@ -0,0 +1,22 @@
+package base;
+
+import cn.hutool.crypto.digest.MD5;
+
+public class Md5Crypto {
+
+    public static String encode(String keyFactor) {
+        return MD5.create().digestHex16(keyFactor);
+    }
+
+    public static String encode32(String keyFactor) {
+        return MD5.create().digestHex(keyFactor);
+    }
+
+    public static void main(String[] args) {
+        String keyFactor = "hello";
+        String decodeKey = MD5.create().digestHex16(keyFactor);
+        String decodeKey2 = MD5.create().digestHex(keyFactor);
+        System.out.println(decodeKey);
+        System.out.println(decodeKey2);
+    }
+}
