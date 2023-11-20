f514bdb4a63d8dbf54dc61aef9e66f262280a128
Laughing <tianjian361@163.com>
2023-11-20 17:27:29 +0800

    update

---
+++ grammar/map/StaticFlowExample.java
@@ -0,0 +1,22 @@
+package map;
+
+public class StaticFlowExample {
+    // 静态成员变量的初始化
+    static int staticVariable = 42;
+
+    // 静态块
+    static {
+        System.out.println("Static block 1");
+        staticVariable = 100;
+    }
+
+    static {
+        System.out.println("Static block 2");
+    }
+
+    public static void main(String[] args) {
+        // 主方法
+        System.out.println("Main method");
+        System.out.println(staticVariable);
+    }
+}
